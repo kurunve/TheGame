3da92339c4a491e7b23850b6bbf4837aeea4d42c
kurunve <VolodymyrAntonuk@gmail.com>
2014-01-27 20:54:07 +0200

    Ania:Task solved

--- Game/Game/Levels/Level1.cs
+++ Game/Game/Levels/Level1.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Text;
 
 namespace Game.Levels
 {
@@ -11,7 +12,34 @@ namespace Game.Levels
 
         public static string Hello(string user_name)
         {
-            return string.Format("{0}, {1}!", "Hello",user_name);
+            bool flag = false;
+            string[] mass = user_name.Split(' ');
+            string name = "";
+            for (int i = 0; i < mass.Length; ++i)
+            {
+                if (mass[i].ToLower() == mass[i])
+                {
+                    string temp = "";
+                    temp += mass[i][0];
+                    temp = temp.ToUpper();
+                    for (int j = 1; j < mass[i].Length; ++j)
+                        temp += mass[i][j];
+
+                    name += temp + " ";
+                    flag = true;
+                }
+
+                else
+                {
+                    name += mass[i];
+                }
+            }
+            if (flag == false)
+                return string.Format("{0}, {1}!", "Hello", name.TrimEnd(' '));
+            else
+            {
+                return string.Format("{0}, {1}.", "Hello", name.TrimEnd(' '));
+            }
         }
 
         public static string FizzBuzzTest(int i)
